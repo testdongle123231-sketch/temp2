ce72dca28d1fa5000f1231cfeaeb50f8dac07f54
parro <hackingtest017@gmail.com>
2025-11-09 10:20:19 +0300

    type fix

--- backend/api/src/middlewares/authorize.ts
+++ backend/api/src/middlewares/authorize.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 
 export function authorize(allowedRoles: string[] | string) {
-    return (req: Request & { user?: { role?: string } }, res: Response, next: NextFunction) => {
+    return (req: Request, res: Response, next: NextFunction) => {
         const userRole = req.user?.role;
         if (userRole && (Array.isArray(allowedRoles) ? allowedRoles.includes(userRole) : allowedRoles === userRole)) {
             next();
